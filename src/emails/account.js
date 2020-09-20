bdc46deabf84332df3605b7c1348fab1d691deaf
jeet adhikari <jeet@backbase.com>
2020-09-21 00:08:49 +0200

    updated tests

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'jeet.adhikari1992@gmail.com',
+        from: 'andrew@mead.io',
         subject: 'Thanks for joining in!',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
     })
